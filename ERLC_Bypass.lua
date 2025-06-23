66580b0766a5a1baa783a094309b8020995f59ad
auto <hitbydev@proton.me>
2025-06-23 15:13:05 +0100

    Create ERLC_Bypass.lua

---
+++ ERLC_Bypass.lua
@@ -0,0 +1,16 @@
+local script_context = game:GetService("ScriptContext")
+local connections = getconnections(script_context.Error)
+
+for _, c in pairs(connections) do
+	local func = c.Function
+	if func then
+		local src = getinfo(func).short_src
+		if string.find(src, "LocalScript") then
+			replaceclosure(func, function()
+				return print("WE SO UD RNN 🔥")
+			end)
+			c:Disable()
+			warn("ULTRA DETECTED BYPASS LOADED 🥶")
+		end
+	end
+end
